ce2785c2ca4d5bc4e91a5063ea55ebe38f014878
eptruong <137592240+eptruong@users.noreply.github.com>
2023-11-21 00:27:54 -0500

    Update pong.ino

--- pong/pong.ino
+++ pong/pong.ino
@@ -1,9 +1,47 @@
 #include <ESP32Lib.h>
+#include <Ressources/Font6x8.h>
 
-void setup() {
+//pin configuration
+const int redPin = 4;
+const int greenPin = 2;
+const int bluePin = 15;
+const int hsyncPin = 14;
+const int vsyncPin = 12;
+   
+//VGA Device
+VGA3Bit vga;
 
-}
+void player_one() {
   
-void loop() {
+}
 
+
+void player_two() {
+  
+}
+
+void player_three() {
+  
+}
+
+void player_four() {
+  
+}
+void setup()
+{
+  //initializing vga at the specified pins
+  vga.init(vga.MODE320x240, redPin, greenPin, bluePin, hsyncPin, vsyncPin);
+  //selecting the font
+  vga.setFont(Font6x8);
+  //displaying the text
+  vga.println("Hello World! Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing ");
+}
+int a = 0;
+void loop()
+{
+  vga.clear(vga.RGB(0));
+  //text position
+  delay(10);
+  vga.setCursor(100, 100);
+  vga.println(a++);
 }
