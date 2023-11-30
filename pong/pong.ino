99f5b109963fb218f6dbce7203ce1bbdc5df62c5
eptruong <137592240+eptruong@users.noreply.github.com>
2023-11-29 22:11:26 -0500

    test
    test

--- pong/pong.ino
+++ pong/pong.ino
@@ -16,11 +16,6 @@ VGA3Bit vga;
 #define ECHO1 16
 #define TRIG2 26
 #define ECHO2 27
-#define TRIG3 ???
-#define ECHO3 ???
-#define TRIG4 ???
-#define ECHO4 ???
-
 
 // Returns distance (cm) from distance sensor
 float read_distance(int sensor) {
@@ -34,14 +29,6 @@ float read_distance(int sensor) {
     case 2:
       trig = TRIG2;
       echo = ECHO2;
-      break;
-    case 3:
-      trig = TRIG3;
-      echo = ECHO3;
-      break;
-    case 4:
-      trig = TRIG4;
-      echo = ECHO4;
       break;
   }
   digitalWrite(trig, LOW); delayMicroseconds(2);
@@ -58,61 +45,49 @@ float read_distance(int sensor) {
 }
 
 
-void draw_paddles(int paddle_coord[], boolean eliminated[]) {
-  int paddle_width = 75;
-  int paddle_height = 20;
-
-  //Player 1
-  if (!eliminated[0]) {
-    vga.fillRect(120, paddle_coord, paddle_height, paddle_width, vga.RGB(255,255,255));
-  }
+void draw_paddles() {
+  int paddle_width = 30;
+  int paddle_height = 10;
+  int player_coord[2];
+  
+  //Convert distance to coords for Player 1
+  player_coord[0] = int((16 - player_dist[0]) / (16 - 4) * (300 - 30));
+  vga.fillRect(20, paddle_coord[0], paddle_height, paddle_width, vga.RGB(255,255,255));
   
   //Player 2
-  if (!eliminated[1]) {
-    vga.fillRect(660, paddle_coord, paddle_height, paddle_width, vga.RGB(255,255,255));
-  }
-  
-  //Player 3
-  if (!eliminated[2]) {
-    vga.fillRect(paddle_coord, 20, paddle_height, paddle_width, vga.RGB(255,255,255));
-  }
-    
-  //Player 4
-  if (!eliminated[3]) {
-    vga.fillRect(paddle_coord, 560, paddle_height, paddle_width, vga.RGB(255,255,255));
-  }
+  player_coord[1] = int((16 - player_dist[1]) / (16 - 4) * (300 - 30));
+  vga.fillRect(360, paddle_coord[1], paddle_height, paddle_width, vga.RGB(255,255,255));
     
 }
 
 int num_index = 10;
-int smoothing_index[4] = {0};
-float distance_readings[4][num_index];
+int smoothing_index[2] = {0};
+float distance_readings[2][num_index];
 
-float* average_smoothing(float player_dist[]) {
-  for (int i = 0; i < 4; i++) {
+void average_smoothing() {
+  for (int i = 0; i < 2; i++) {
     distance_readings[i][smoothing_index[i]++] = player_dist[i]; 
     smoothing_index[i] = (smoothing_index[i]) % numIndex;
   }
 
-  float sum[4];
-  for (int i = 0; i < 4; i++) {
+  float sum[2];
+  for (int i = 0; i < 2; i++) {
     for int(j = 0; j < num_index; j++) {
       sum[i] += distance_readings[i][j];
     }
     player_dist[i] = sum[i]/10;
   }
-  return player_dist;
 }
 
 // Converts distance to coordinate on playing field
-//int* convert_distance(float player_dist[]) {
-//  //TODO Nathan
-//}
+int* convert_distance(float player_dist[]) {
+  //TODO Nathan
+}
 
 
 int a = 20;
-int x = 100 + 
-int y = 300
+//int x = 100 + 
+int y = 300;
 int delta = 2;
 void animate() {
   // Ball touches left border
@@ -121,7 +96,7 @@ void animate() {
       dx = delta;
     }
     else {
-      player_score[0]--;
+      player_lives[0]--;
       new_round();
     }
   }
@@ -135,7 +110,7 @@ void animate() {
       dx=-delta;
     }
     else {
-      player_score[1]--;
+      player_lives[1]--;
       new_round();
     }
   }
@@ -144,56 +119,35 @@ void animate() {
 
   // Ball touches top border
   if(y+a>=600) {
-    if (eliminated[2] {
-      dy = -delta; 
-    }
-    else {
-      player_score[2]--;
-      new_round();
-    }
-    
+    dy = -delta; 
   }
   
   // Ball touches bottom border
   if(y<=0) {
-    if (eliminated[3]) {
-      dy = delta;
-    }
-    else {
-      player_score[3]--;
-      new_round();
-    }
+    dy = delta;
   }
 
   x+=dx;  y+=dy;
   vga.fillRect(x, y, 20, 20, vga.RGB(255, 255, 255));
 }
 
-void new_round() {
-  
-}
+//void new_round() {
+//  
+//}
 
-boolean four_player = true; // Current state of gamemode
-boolean eliminated[4] = {0}; // Track who is eliminated (Players 3 & 4 eliminated if four_player = false)
+
 boolean started = false;
-int player_score[4] = {5};
+int player_lives[2] = {5};
 
 void setup()
 {
-  vga.init(vga.MODE800x600, redPin, greenPin, bluePin, hsyncPin, vsyncPin);
+  vga.setFrameBufferCounter(2);
+  vga.init(vga.MODE400x300, redPin, greenPin, bluePin, hsyncPin, vsyncPin);
   vga.setFont(Font6x8);
 
-  if (!four_player) {
-      eliminated[2] = true;
-      eliminated[3] = true;
-  }
-  
   //TODO for Natasha:
-    // Originally 4 player, button click changes to 2 player
-    // Ask user if they are ready with the option to alternate between 2/4 player
-    // Print on screen the current mode (2 or 4) 
-    // Helpful functions you should use: vga.setCursor(x,y) and vga.print("text") 
-  
+  // Ask user if they are ready 
+  // Helpful functions you should use: vga.setCursor(x,y) and vga.print("text") 
 }
 
 
@@ -206,15 +160,14 @@ void loop()
   else {
     vga.clear(vga.RGB(0));
     delay(10);
-    //  float player_dist[4]; // [0...n-1] where index i is Player i + 1
-    //  for (int i = 0; i < 4; i++) {
-    //    player_dist[i] = read_distance(i + 1);
-    //  }
+    float player_dist[2];
+    for (int i = 0; i < 2; i++) {
+      player_dist[i] = read_distance(i + 1);
+    }
     
-      
-    //  float smooth_player_dist[] = average_smoothing(player_dist);
-    //  draw_paddles(convert_distance(smooth_player_dist), eliminated);
-    //
+    average_smoothing();
+    draw_paddles(convert_distance(player_dist));
+    
   
       
     vga.show();
